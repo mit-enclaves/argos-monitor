740ea5088163bf1aad8bce946b0ad2b94c173783
Adrien Ghosn <ghosn.adrien@gmail.com>
2022-07-16 12:31:03 +0200

    Rawc with bss and ro segments

--- guest/main.c
+++ guest/main.c
@@ -1,6 +1,11 @@
 // Tell the compiler incoming stack alignment is not RSP%16==8 or ESP%16==12
+
+const int RO = 0x888;
+int bss;
+
 __attribute__((force_align_arg_pointer))
 void _start() {
+  bss = RO;
   asm("movl $0x666, %eax;"
       "movl $0x777, %ebx;"
       "vmcall");
