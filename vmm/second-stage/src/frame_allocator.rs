58f4bcbece922f65a8cdb75e6de55389d5922e7a
walterieux <david.desboeufs@hotmail.fr>
2022-09-21 13:40:25 +0200

    deallocate_frame first implementation

--- vmm/second-stage/src/frame_allocator.rs
+++ vmm/second-stage/src/frame_allocator.rs
@@ -1,9 +1,8 @@
 //! Linked list allocator
-//! inpired from allocator.rs from aghosn_stage2 branch and https://os.phil-opp.com/allocator-designs/
+//! Inpired from allocator.rs from aghosn_stage2 branch and https://os.phil-opp.com/allocator-designs/
 
 // TODO check for safe static operations
 
-use std::mem;
 use vmx::{Frame, HostPhysAddr};
 
 const PAGE_SIZE: u64 = 0x1000;
@@ -16,7 +15,6 @@ pub struct Page {
 }
 
 pub struct PageInfo {
-    pub is_free: bool,
     pub frame_id: usize,
     pub next_free: Option<&'static mut PageInfo>,
 }
@@ -25,7 +23,7 @@ static mut MEMORY_PAGES: [Page; NB_PAGES] = [Page {
     data: [0; PAGE_SIZE as usize],
 }; NB_PAGES];
 
-// linked list stored as an array
+// Linked list stored as an array
 static mut LINKED_LIST_FREE_PAGES: [PageInfo; NB_PAGES] = std::mem::uninitialized(); // TODO: DEPRECATED
 
 pub struct FrameAllocator {
@@ -47,13 +45,11 @@ impl FrameAllocator {
     pub fn init() {
         for i in 0..NB_PAGES - 1 {
             LINKED_LIST_FREE_PAGES[i] = PageInfo {
-                is_free: true,
                 frame_id: i,
                 next_free: Some(&mut LINKED_LIST_FREE_PAGES[i + 1]),
             }
         }
         LINKED_LIST_FREE_PAGES[NB_PAGES - 1] = PageInfo {
-            is_free: true,
             frame_id: NB_PAGES - 1,
             next_free: None,
         }
@@ -63,7 +59,7 @@ impl FrameAllocator {
         let curr_head = self.head;
         match curr_head {
             Some(x) => {
-                self.head = x.next_free; // move the head to the next free page
+                self.head = x.next_free; // Move head to the next free page
                 Some(x.frame_id)
             }
             None => None,
@@ -74,7 +70,6 @@ impl FrameAllocator {
         let id = Self::allocate_frame_get_id(&mut self);
         match id {
             Some(x) => {
-                // TODO check page is free
                 let addr = &MEMORY_PAGES[x] as *const _ as *mut u8;
                 let phys_addr = (addr as u64) - self.virt_offset + self.phys_offset;
                 return Some(Frame {
@@ -86,7 +81,17 @@ impl FrameAllocator {
         }
     }
 
-    pub fn deallocate_frame(frame: Frame) {
-        unimplemented!();
+    pub fn deallocate_frame(&mut self, frame: Frame) {
+        let id = frame.virt_addr - &MEMORY_PAGES[0];
+        if 0 < id || id <= NB_PAGES { // Simply return if index is out of bounds
+            return;
+        }
+
+        // Replace the head with the new freed frame
+        let old_head = self.head;
+        // TODO: how to put back the new free block in the linked list? For now I use
+        // the fact that we have a 1 1 mapping between MEMORY_PAGES and LINKED_LIST_FREE_PAGES.
+        self.head = Some(&mut LINKED_LIST_FREE_PAGES[id]);
+        LINKED_LIST_FREE_PAGES[id].next_free = old_head;
     }
 }
