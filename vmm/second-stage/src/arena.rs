91dc2ba75a978dd7c8ca551d04f3e5ad64712dc1
walterieux <david.desboeufs@hotmail.fr>
2022-09-21 11:14:08 +0200

    Enable re-using objects allocated from TypedArena
    The old TypedArena was a bump allocator, meaning that objects can't be
    freed. This commit introduces a new lined-list based design that allow
    re-using objects while keeping a finite interface.

--- vmm/second-stage/src/arena.rs
+++ vmm/second-stage/src/arena.rs
@@ -16,6 +16,16 @@ pub trait ArenaItem {
 
 // —————————————————————————————— Typed Arena ——————————————————————————————— //
 
+/// Free list node.
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum NextFree {
+    /// This block is free, and the next free is at the given index.
+    Free(u32),
+
+    /// This block is not free.
+    NotFree,
+}
+
 /// A typed arena, from which objects can be dynamicalle allocated and freed.
 pub struct TypedArena<T, const N: usize>
 where
@@ -24,8 +34,11 @@ where
     /// The baking store from which objects are allocated.
     store: [T; N],
 
-    /// For now we use a simple bump allocator, we will move to a better allocator later.
-    bumper: usize,
+    /// The free list, where free_list[n] returns the index of the next free object.
+    free_list: [NextFree; N],
+
+    /// The next free block, if any.
+    head: u32,
 }
 
 impl<T, const N: usize> TypedArena<T, N>
@@ -33,26 +46,47 @@ where
     T: ArenaItem,
 {
     pub const fn new(store: [T; N]) -> Self {
-        Self { store, bumper: 0 }
+        let mut free_list = [NextFree::NotFree; N];
+        let mut i = 0;
+        while i < free_list.len() {
+            let next = (i as u32 + 1) % (free_list.len() as u32);
+            free_list[i] = NextFree::Free(next);
+            i += 1;
+        }
+        Self {
+            store,
+            free_list,
+            head: 0,
+        }
     }
 
     pub fn allocate(&mut self) -> Result<Handle<T, N>, T::Error> {
-        if self.bumper >= self.store.len() {
-            return Err(T::ALLOCATION_ERROR);
+        let head = self.head as usize;
+        match self.free_list[head] {
+            NextFree::Free(next) => {
+                self.head = next;
+                self.free_list[head] = NextFree::NotFree;
+                Ok(Handle {
+                    idx: head,
+                    _type: PhantomData,
+                })
+            }
+            NextFree::NotFree => Err(T::ALLOCATION_ERROR),
         }
-
-        let handle = Handle {
-            idx: self.bumper,
-            _type: PhantomData,
-        };
-        self.bumper += 1;
-
-        Ok(handle)
     }
 
     #[allow(unused)]
     pub fn free(&mut self, _object: Handle<T, N>) {
-        // TODO
+        let idx = _object.idx();
+        
+        // Safety check
+        if self.free_list[idx] != NextFree::NotFree {
+            panic!("Trying to free an already free object");
+        }
+
+        // Insert back into free list
+        self.free_list[idx] = NextFree::Free(self.head);
+        self.head = idx as u32;
     }
 }
 
