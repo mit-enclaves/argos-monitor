e74659bf5bd8a25bc3ddf0d4d4ad4b9ae350069a
YuChen Qian <yuchenqian96@gmail.com>
2023-03-07 17:29:16 +0100

    linux: print console to serial port
    Signed-off-by: YuChen Qian <yuchenqian96@gmail.com>

--- monitor/first-stage/src/guests/linux.rs
+++ monitor/first-stage/src/guests/linux.rs
@@ -37,7 +37,7 @@ static COMMAND_LINE: &'static [u8] =
     b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0 console=ttyS0\0";
 #[cfg(feature = "bare_metal")]
 static COMMAND_LINE: &'static [u8] =
-    b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0,115200 nr_cpus=2\0";
+    b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0,115200 console=ttyS0,115200 nr_cpus=2\0";
 
 pub struct Linux {}
 
