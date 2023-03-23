63a8863b09d0f3134e256aea22cda915b4f07246
YuChen Qian <yuchenqian96@gmail.com>
2023-03-23 15:50:14 +0100

    guest/linux: dump scheduler
    Signed-off-by: YuChen Qian <yuchenqian96@gmail.com>

--- vmm/first-stage/src/guests/linux.rs
+++ vmm/first-stage/src/guests/linux.rs
@@ -35,10 +35,10 @@ const SETUP_HDR: u64 = 0x1f1;
 // WARNING: Don't forget that the command line must be null terminated ('\0')!
 #[cfg(not(feature = "bare_metal"))]
 static COMMAND_LINE: &'static [u8] =
-    b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0 console=ttyS0 nokaslr\0";
+    b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0 console=ttyS0 nokaslr sched_verbose\0";
 #[cfg(feature = "bare_metal")]
 static COMMAND_LINE: &'static [u8] =
-    b"root=/dev/sdb2 apic=debug show_lapic=all nokaslr earlyprintk=serial,ttyS0,115200 console=ttyS0,115200 pci=earlydump nr_cpus=1\0";
+    b"root=/dev/sdb2 apic=debug show_lapic=all nokaslr earlyprintk=serial,ttyS0,115200 console=ttyS0,115200 pci=earlydump nr_cpus=1 sched_verbose\0";
 
 pub struct Linux {}
 
