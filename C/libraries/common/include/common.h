2fa65901fb558d7207455ceac8d47e0ca0f39e1e
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-03-31 16:52:13 +0100

    Adding common library

---
+++ C/libraries/common/include/common.h
@@ -0,0 +1,21 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TEST(cond)                                                   \
+  do {                                                               \
+    if (!(cond)) {                                                   \
+      fprintf(stderr, "[%s:%d] %s\n", __FILE__, __LINE__, __func__); \
+      abort();                                                       \
+    }                                                                \
+  } while (0);
+
+#define LOG(...)                                          \
+  do {                                                    \
+    printf("[%s:%d] %s: ", __FILE__, __LINE__, __func__); \
+    printf(__VA_ARGS__);                                  \
+    printf("\n");                                         \
+  } while (0);
+
+#endif
