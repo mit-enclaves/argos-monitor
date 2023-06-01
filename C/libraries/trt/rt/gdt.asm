4cd2a577fdc5e7cb5f844c28ff66b8ffbf9e2ae5
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-06-01 11:01:13 +0100

    Implementation of segment & syscalls
    This C folder provides code snippets to manipulate the configuration of
    ring 0 inside the domain.
    This will probably not be the final version of the TRT, which we will
    probably end up writting in Rust, but rather an exploratory
    implementation so that we have reference code to manipulate all the
    necessary registers.
    
    Next: I will implement some user space transition probably.

--- C/libraries/trt/rt/gdt.asm
+++ C/libraries/trt/rt/gdt.asm
@@ -4,15 +4,14 @@ gdt_reload:
     mov rbp, rsp
     mov ax, dx
     pushfq
-    cli
     lgdt [rdi]
     popfq
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
+    ;mov ds, ax
+    ;mov es, ax
+    ;mov ss, ax
     pop rbp
-    pop rax
-    push rsi
-    push rax
+    ;pop rax
+    ;push rsi
+    ;push rax
     xor rax, rax
-    o64 retf
+    ret
