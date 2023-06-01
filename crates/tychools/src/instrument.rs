f0c723052dded1235185916d4157229725297a27
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-06-01 16:34:20 +0100

    Saving tychools progress bin instrumentation
    I changed the binary to take two ELFs, a user and a kernel one, and to
    have generic support for two types of operations:
    1. Move a section into its own segment
    2. Add a nodata segment (useful for stacks, shared buffers, etc.).
    
    Missing: What's missing right now is the ability to merge two binaries
    into a single ELF. I could simply add the segments and be done with it,
    but I'd like to have a valid output for sections as well if possible...

--- crates/tychools/src/instrument.rs
+++ crates/tychools/src/instrument.rs
@@ -1,15 +1,47 @@
-use std::fs::OpenOptions;
-use std::io::Write;
 use std::path::PathBuf;
 
 use log::info;
-use object::Endianness;
+use serde::{Deserialize, Serialize};
+use serde_json;
 
 use crate::allocator::PAGE_SIZE;
 use crate::elf_modifier::{ModifiedELF, TychePhdrTypes};
 use crate::page_table_mapper::generate_page_tables;
 
 pub const DEFAULT_STACK_SIZE: usize = 0x5000;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SegmentDescriptor {
+    start: Option<u64>,
+    size: usize,
+    tpe: TychePhdrTypes,
+    write: bool,
+    exec: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum BinaryOperation {
+    SectionToSegment(String, TychePhdrTypes),
+    AddSegment(SegmentDescriptor),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BinaryInstrumentation {
+    /// Path to the binary.
+    path: String,
+    /// Extra operations to perform on the binary.
+    ops: Option<Vec<BinaryOperation>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Manifest {
+    /// User binary
+    user_bin: Option<BinaryInstrumentation>,
+    /// Kernel binary
+    kern_bin: Option<BinaryInstrumentation>,
+    /// Destination ELF file.
+    output: String,
+}
 
 pub fn modify_binary(src: &PathBuf, dst: &PathBuf) {
     let data = std::fs::read(src).expect("Unable to read source file");
@@ -19,14 +51,14 @@ pub fn modify_binary(src: &PathBuf, dst: &PathBuf) {
     // Move default shared buffer into its own segment.
     elf.split_segment_at_section(
         ".tyche_shared_default_buffer",
-        TychePhdrTypes::Shared as u32,
+        TychePhdrTypes::KernelShared as u32,
     )
     .expect("Failed to split section into segment");
 
     // Add the enclave stack as a segment.
     elf.append_nodata_segment(
         None,
-        TychePhdrTypes::EnclStack as u32,
+        TychePhdrTypes::KernelStack as u32,
         object::elf::PF_R | object::elf::PF_W,
         DEFAULT_STACK_SIZE,
     );
@@ -45,15 +77,100 @@ pub fn modify_binary(src: &PathBuf, dst: &PathBuf) {
     );
 
     // Let's write that thing out.
-    let mut out: Vec<u8> = Vec::with_capacity(elf.len());
-    let mut writer = object::write::elf::Writer::new(Endianness::Little, true, &mut out);
-    elf.dump(&mut writer);
+    //let mut out: Vec<u8> = Vec::with_capacity(elf.len());
+    //let mut writer = object::write::elf::Writer::new(Endianness::Little, true, &mut out);
+    elf.dump(dst);
 
-    let mut file = OpenOptions::new()
+    /*let mut file = OpenOptions::new()
         .create(true)
         .read(true)
         .write(true)
         .open(dst)
         .expect("Unable to open dest file");
-    file.write(&*out).expect("Unable to dump the content");
+    file.write(&*out).expect("Unable to dump the content");*/
+}
+
+pub fn instrument_with_manifest(src: &PathBuf) {
+    // Parse the manifest.
+    let manifest: Manifest = {
+        let data = std::fs::read(src).expect("Unable to read source file");
+        let content = String::from_utf8(data).expect("Unable to convert data to string");
+        serde_json::from_str(&content).expect("Failed to parse JSON")
+    };
+    instrument_binary(&manifest);
+}
+
+/// Parse singular binary instrumentation description and applies its operations.
+pub fn parse_binary(binary: &BinaryInstrumentation) -> Box<ModifiedELF> {
+    let data = std::fs::read(PathBuf::from(&binary.path)).expect("Unable to read the binary");
+    let mut elf = ModifiedELF::new(&*data);
+
+    // Apply all the operations.
+    if let Some(operations) = &binary.ops {
+        for op in operations {
+            match &op {
+                BinaryOperation::SectionToSegment(section, tpe) => {
+                    elf.split_segment_at_section(section, *tpe as u32)
+                        .expect("Unable to perform the desired split operation");
+                }
+                BinaryOperation::AddSegment(descr) => {
+                    let mut rights = object::elf::PF_R;
+                    if descr.write {
+                        rights |= object::elf::PF_W;
+                    }
+                    if descr.exec {
+                        rights |= object::elf::PF_X;
+                    }
+                    elf.append_nodata_segment(descr.start, descr.tpe as u32, rights, descr.size);
+                }
+            }
+        }
+    }
+    elf
+}
+
+pub fn instrument_binary(manifest: &Manifest) {
+    // Parse the user binary if present.
+    let mut user_elf = if let Some(user) = &manifest.user_bin {
+        let mut bin = parse_binary(user);
+        bin.mark(TychePhdrTypes::UserConfidential);
+        Some(bin)
+    } else {
+        None
+    };
+    // Parse the kernel binary if present.
+    let kern_elf = if let Some(kern) = &manifest.kern_bin {
+        let mut bin = parse_binary(kern);
+        bin.mark(TychePhdrTypes::KernelConfidential);
+        Some(bin)
+    } else {
+        None
+    };
+
+    // Complex case.
+    if let (Some(ref mut user), Some(kern)) = (&mut user_elf, &kern_elf) {
+        let user = user.as_mut();
+        let kern = kern.as_ref();
+        if user.overlap(kern) {
+            panic!("The two binaries overlap");
+        }
+        user.merge(kern);
+        user.generate_page_tables();
+        user.dump(&PathBuf::from(&manifest.output));
+        return;
+    }
+    // Simpler cases
+    if let Some(mut user) = user_elf {
+        user.generate_page_tables();
+        user.dump(&PathBuf::from(&manifest.output));
+        return;
+    }
+
+    if let Some(mut kern) = kern_elf {
+        kern.generate_page_tables();
+        kern.dump(&PathBuf::from(&manifest.output));
+        return;
+    }
+
+    panic!("Should never reach");
 }
