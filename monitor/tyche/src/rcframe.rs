5711549d193a64f98d47d6a3983fd9a36fc120b6
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-07-17 17:32:13 +0100

    Fixing just checks

--- monitor/tyche/src/rcframe.rs
+++ monitor/tyche/src/rcframe.rs
@@ -8,21 +8,23 @@ use crate::statics::NB_CORES;
 
 /// Simple wrapper for frames representing a frame.
 /// This structure will be inside a static array in the monitor.
+#[allow(dead_code)]
 pub struct RCFrame {
     count: usize,
     pub frame: Frame,
 }
 
 impl RCFrame {
+    #[allow(dead_code)]
     pub fn new(frame: Frame) -> Self {
         Self { count: 1, frame }
     }
-
+    #[allow(dead_code)]
     pub fn acquire(&mut self) -> usize {
         self.count += 1;
         self.count
     }
-
+    #[allow(dead_code)]
     pub fn release(&mut self) -> Result<usize, CapaError> {
         if self.count == 0 {
             return Err(CapaError::InvalidOperation);
@@ -35,8 +37,10 @@ impl RCFrame {
 
 // ————————————————————— Create a pool for such objects ————————————————————— //
 
+#[allow(dead_code)]
 pub type RCFramePool = GenArena<RCFrame, { NB_DOMAINS * NB_CORES }>;
 
+#[allow(dead_code)]
 pub fn drop_rc(pool: &mut RCFramePool, v: Handle<RCFrame>) {
     if v.is_invalid() {
         return;
@@ -53,6 +57,7 @@ pub fn drop_rc(pool: &mut RCFramePool, v: Handle<RCFrame>) {
     }
 }
 
+#[allow(dead_code)]
 pub const EMPTY_RCFRAME: RCFrame = RCFrame {
     count: 0,
     frame: Frame {
