f9353646fdaaa450ded9697241596737343bc55c
papa <pavle.jane@gmail.com>
2023-07-25 16:56:46 +0200

    Refactoring to hash only chose segments, after flag is set. Done it in tychools, need to do it in Tyche

--- crates/tychools/src/attestation.rs
+++ crates/tychools/src/attestation.rs
@@ -5,35 +5,6 @@ use sha2::{Digest, Sha256};
 
 use crate::elf_modifier::{ModifiedELF, ModifiedSegment, TychePhdrTypes, DENDIAN};
 
-fn hash_segment_data(enclave : & Box<ModifiedELF>, hasher : & mut Sha256) {
-    let mut real_size = 0;
-    for seg in &enclave.segments {
-        if ModifiedSegment::is_loadable(seg.program_header.p_type(DENDIAN)) {
-            if let Some(tpe) = TychePhdrTypes::from_u32(seg.program_header.p_type(DENDIAN)) {
-                let memsz = seg.program_header.p_memsz(DENDIAN);
-                let align = seg.program_header.p_align(DENDIAN);
-                log::info!("Sz from the header {:#x}", memsz);
-                log::info!("Align from the header {:#x}", align);
-                log::info!("Size of the region {:#x}", seg.data.len());
-                for u8_data in &seg.data {
-                    let arr_u8 : [u8;1] = [*u8_data];
-                    hasher.input(&arr_u8);
-                }
-                let mut diff = (memsz + align - 1) / align * align;
-                real_size+=diff;
-                log::info!("Alligned address {:#x}", diff);
-                diff = diff - (seg.data.len() as u64);
-                log::info!("Diff - number of zeros to be added to the hash {:#x}", diff);
-                for _ in 0..diff {
-                    let arr_u8 : [u8;1] = [0];
-                    hasher.input(&arr_u8);
-                }
-            }
-        }
-    }
-    log::info!("Real size {:#x}", real_size);
-}
-
 fn hash_acc_rights(hasher : & mut Sha256, flags : u32, mask : u32) {
     if flags & mask != 0 {
         log::trace!("1");
@@ -48,12 +19,14 @@ fn hash_acc_rights(hasher : & mut Sha256, flags : u32, mask : u32) {
 static PF_X : u32 = 1 << 0;
 static PF_W : u32 = 1 << 1;
 static PF_R : u32 = 1 << 2;
+static PF_H : u32 = 1 << 3;
 
 //todo why are segments not in order
 fn hash_segments_info(enclave : & Box<ModifiedELF>, hasher : & mut Sha256, offset : u64) {
     let dom_id : u64=2;
     hasher.input(&u64::to_le_bytes(dom_id));
     let mut segment_off = offset;
+    let mut real_size = 0;
     for seg in &enclave.segments {
         if ModifiedSegment::is_loadable(seg.program_header.p_type(DENDIAN)) {
             if let Some(tpe) = TychePhdrTypes::from_u32(seg.program_header.p_type(DENDIAN)) {
@@ -63,29 +36,34 @@ fn hash_segments_info(enclave : & Box<ModifiedELF>, hasher : & mut Sha256, offse
                 let sz = (memsz + align - 1) / align * align;
                 log::trace!("Region start {:#x}", start);
                 log::trace!("Region end {:#x}", start + sz);
-                hasher.input(&u64::to_le_bytes(start));
-                hasher.input(&u64::to_le_bytes(start + sz));
-
-                if tpe.is_confidential() {
-                    log::trace!("Conf = 1");
-                    hasher.input(&u8::to_le_bytes(1 as u8));
-                }
-                else {
-                    log::trace!("Conf = 0");
-                    hasher.input(&u8::to_le_bytes(0 as u8));
-                }
-
                 let flags = seg.program_header.p_flags(DENDIAN);
-                log::trace!("X right");
-                hash_acc_rights(hasher, flags, PF_X);
-                log::trace!("W right");
-                hash_acc_rights(hasher, flags, PF_W);
-                log::trace!("R right");
-                hash_acc_rights(hasher, flags, PF_R);
-
+                let mut diff = (memsz + align - 1) / align * align;
+                real_size+=diff;
+                log::info!("Alligned address {:#x}", diff);
+                diff = diff - (seg.data.len() as u64);
+                log::info!("Diff - number of zeros to be added to the hash {:#x}", diff);
                 log::trace!("Attestation right");
-                hash_acc_rights(hasher, flags, 1 << 3);
-
+                let should_hash = (flags & PF_H) != 0;
+                log::trace!("{}", should_hash);
+                if should_hash {
+                    hasher.input(&u64::to_le_bytes(start));
+                    hasher.input(&u64::to_le_bytes(start + sz));
+                    hash_acc_rights(hasher, flags, PF_H);
+                    log::trace!("X right");
+                    hash_acc_rights(hasher, flags, PF_X);
+                    log::trace!("W right");
+                    hash_acc_rights(hasher, flags, PF_W);
+                    log::trace!("R right");
+                    hash_acc_rights(hasher, flags, PF_R);
+                    for u8_data in &seg.data {
+                        let arr_u8 : [u8;1] = [*u8_data];
+                        hasher.input(&arr_u8);
+                    }
+                    for _ in 0..diff {
+                        let arr_u8 : [u8;1] = [0];
+                        hasher.input(&arr_u8);
+                    }
+                }
                 segment_off+=sz;
             }
         }
@@ -97,8 +75,6 @@ pub fn attest(src: &PathBuf, offset: u64) {
     let mut hasher = Sha256::default();
     let mut enclave = ModifiedELF::new(&data);
     enclave.fix_page_tables(offset);
-    
-    hash_segment_data(&enclave, &mut hasher);
 
     hash_segments_info(&enclave, &mut hasher, offset);
     
