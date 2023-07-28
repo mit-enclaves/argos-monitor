a250df0c8eb8cf4ca9ad7b8d8a03b2e905f7da5e
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-07-28 08:51:15 +0100

    Making the test-trt a bit more resilient

--- C/libraries/tyche-trusted-runtime/load/main.c
+++ C/libraries/tyche-trusted-runtime/load/main.c
@@ -1,11 +1,34 @@
 #include "common.h"
 #include "sdk_tyche.h"
 
+/// Looks up for the shared memory region with the enclave.
+static void* find_default_shared(tyche_domain_t* enclave)
+{
+  domain_shared_memory_t* shared_sec = NULL;
+  if (enclave == NULL) {
+    ERROR("Supplied enclave is null.");
+    goto failure;
+  }
+  // Find the shared region.
+  dll_foreach(&(enclave->config.shared_regions), shared_sec, list) {
+      if (shared_sec->segment->p_type == KERNEL_SHARED 
+          && shared_sec->segment->p_vaddr == 0x300000) {
+        return (void*)(shared_sec->untrusted_vaddr);
+      }
+  }
+  ERROR("Unable to find the shared buffer for the enclave!");
+failure:
+  return NULL;
+}
+
 int main(int argc, char* argv[])
 {
   tyche_domain_t enclave;
   LOG("Loading enclave");
   /// Enable divide by zero exception.
+  /// The code in the RT will trigger an exception if it's a div by zero it
+  /// will write 666 in the shared memory buffer and return to us.
+  /// If any other handler is called, it will hlt.
   if (sdk_create_domain(
         &enclave, argv[0], 1, 1, DEFAULT_PERM, CopyVCPU) != SUCCESS) {
     ERROR("Unable to parse the enclave");
@@ -21,6 +44,8 @@ int main(int argc, char* argv[])
 
   /// We survived one call!
   LOG("Survived a call to the enclave!");
+  int* shared = (int*) find_default_shared(&enclave);
+  TEST(*shared == 666);
 
   /// Clean up.
   if (sdk_delete_domain(&enclave) != SUCCESS) {
