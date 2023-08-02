6d6f076c4f135f502ec2ec549269189c24ed1302
papa <pavle.jane@gmail.com>
2023-08-02 17:46:29 +0200

    Design doc for attestation, signing in Tyche works, client side next

--- crates/attestation/src/signature.rs
+++ crates/attestation/src/signature.rs
@@ -1,8 +1,3 @@
-extern crate alloc;
-
-use core::alloc::GlobalAlloc;
-use alloc::alloc::Layout;
-
 pub type DeviceKey = u128;
 pub type DevicePublicKey = DeviceKey;
 pub type DevicePrivateKey = DeviceKey;
@@ -24,32 +19,18 @@ pub struct EnclaveReport {
     pub nonce : u64,
 }
 // use rsa::{RsaPrivateKey, RsaPublicKey, Pkcs1v15Encrypt};
-
+use ed25519_compact::{KeyPair, PublicKey, Seed, Noise};
 pub fn get_attestation_keys() -> (AttestationKey, AttestationKey) { 
-    // let bits = 16;
-    // let mut rng = RndTyche{};
-    // let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-    // let pub_key = RsaPublicKey::from(&priv_key);
+    let msg = b"test";
+    let key_pair = KeyPair::from_seed(Seed::default());
 
-    // // Encrypt
-    // let data = b"hello world";
-    // log::trace!("data");
-    // for x in data {
-    //     log::trace!("u8 {:#x}", x);
-    // }
-    // let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
-    // log::trace!("Enc data");
-    // for x in &enc_data {
-    //     log::trace!("u8 {:#x}", *x);
-    // }
-
-    // // Decrypt
-    // let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
-    // log::trace!("Dec data");
-    // for x in &dec_data {
-    //     log::trace!("u8 {:#x}", *x);
-    // }
-
+    let s = key_pair.sk.sign(msg, Some(Noise::default()));
+    if let Ok(r) = key_pair.pk.verify(msg, &s) {
+        log::trace!("Message verified");
+    }
+    else {
+        log::trace!("Didn't verify the message");
+    }
     (0,0)
 }
 
@@ -60,65 +41,3 @@ pub fn sign_attestation_data(_data : &[u8], _key : AttestationKey) -> Attestatio
 pub fn sign_by_device(_data : &[u8], _key : DevicePrivateKey) -> AttestationSignature {
     0
 }
-
-use rand_core::{RngCore, Error, CryptoRng};
-
-pub struct RndTyche {
-    
-}
-
-impl RngCore for RndTyche {
-    fn next_u32(&mut self) -> u32 {
-        log::trace!("next u32");
-        0
-    }  
-    fn next_u64(&mut self) -> u64 {
-        log::trace!("next u64");
-        0
-    }
-    fn fill_bytes(&mut self, dest: &mut [u8]) {
-        log::trace!("fill bytes");
-        let mut cnt : u8 = 0;
-        for x in dest {
-            *x = cnt;
-            cnt+=1;
-        }
-    }
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
-        log::trace!("try fill bytes");
-        let mut cnt : u8 = 0;
-        for x in dest {
-            *x = cnt;
-            cnt+=1;
-        }
-        Ok(())
-    }
-}
-
-impl CryptoRng for RndTyche {
-
-}
-
-static SZ : usize = 0x5000;
-static mut ARR : [u8;0x5000] = [0;0x5000];
-static mut INDEX : usize = 0;
-
-#[derive(Default)]
-pub struct Allocator;
-
-unsafe impl GlobalAlloc for Allocator {
-     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        log::trace!("allocating");
-        log::trace!("{:#x}", layout.size());
-        INDEX = (INDEX + layout.size()) % SZ;
-        unsafe {
-            let x = (&ARR[INDEX]) as * const u8;
-            x as * mut u8
-        }
-
-     }
-     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
-}
-
-#[global_allocator]
-static GLOBAL_ALLOCATOR: Allocator = Allocator;
--- design/009-attestation.md
+++ design/009-attestation.md
@@ -15,7 +15,23 @@ Added flag to Tychools binary to be able to tell whether we want certain segment
 Added Tyche call for getting back the hash (similar call will be used for attestation), with code 'ENCLAVE_ATTESTATION = 14'. 
 
 Tyche will have pair of keys representing device keys, coming from Root of Trust probably. Private device key is used to sign public attestation key, and public attestaion key is used to sign enclave hash, offset and data.
-Attestation is done through the enclave attestation call, in a way that client is sending a nonce as a challenge, and Tyche responds with a report. Report consists of signed attestation key, and data signed by public attestation key.
+Attestation is done through the enclave attestation call, in a way that client is sending a nonce to enclave as a challenge, enclave calls Tyche and Tyche responds with a report. Report should consist of signed attestation key, and data signed by public attestation key, but for now it is without signatures.
+
+Schema of how it works, without signature so far
+
+```
+[user space]
+Client (has physical offset and generates a nonce)          [checks the hash]
+| [puts args in shared buffer]                                     ^
+-------------------------------------------------------------------|
+| [kernel space]            [reads from shared buff]    [puts results in shared buff, returns]
+|----(call the enclave)----> enclave -----|                        ^
+----------ENCLAVE_ATTESTATION CALL--------|------------------------|
+[Tyche]                                   V [creates the report]   |
+                                            -----------------------|
+```
+
+Expantion could be for client to do the same thing as tychools/attestation but is there need for it? Next step is creating the signature of the message.
 
 ## Next steps
 
