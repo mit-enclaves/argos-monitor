ec17eab518579766fdae2565524594a6bf34b105
papa <pavle.jane@gmail.com>
2023-08-09 13:33:44 +0200

    Fixing PR comments - Enclave code linked with capa library instead of copying code from capa library (Adrien)

--- C/libraries/sdktyche/example/simple-enclave/trusted/main.c
+++ C/libraries/sdktyche/example/simple-enclave/trusted/main.c
@@ -1,45 +1,14 @@
 #include "sdk_tyche_rt.h"
-#include "enclave_app.h"
 #include "tyche_api.h"
-
+#include "enclave_app.h"
 // ———————————————————————————————— Globals ————————————————————————————————— //
 config_t* shared = NULL;
-#define CALC_REPORT 0
-#define READ_REPORT 1
 
 // ————————————————————————— HELLO_WORLD Functions —————————————————————————— //
 
 const char* message = "Hello World!\n\t\0";
 const char* message2 = "Bye Bye! :)!\n\t\0";
 const char* message3 = "Done attestation!\n\t\0";
-
-/// Simple generic vmcall implementation.
-int tyche_call_enclave(vmcall_frame_t* frame)
-{
-  usize result = FAILURE;
-  asm volatile(
-    // Setting arguments.
-    "movq %7, %%rax\n\t"
-    "movq %8, %%rdi\n\t"
-    "movq %9, %%rsi\n\n"
-    "movq %10, %%rdx\n\t"
-    "movq %11, %%rcx\n\t"
-    "movq %12, %%r8\n\t"
-    "movq %13, %%r9\n\t"
-    "vmcall\n\t"
-    // Receiving results.
-    "movq %%rax, %0\n\t"
-    "movq %%rdi, %1\n\t"
-    "movq %%rsi, %2\n\t"
-    "movq %%rdx, %3\n\t"
-    "movq %%rcx, %4\n\t"
-    "movq %%r8,  %5\n\t"
-    "movq %%r9,  %6\n\t"
-    : "=rm" (result), "=rm" (frame->value_1), "=rm" (frame->value_2), "=rm" (frame->value_3), "=rm" (frame->value_4), "=rm" (frame->value_5), "=rm" (frame->value_6)
-    : "rm" (frame->vmcall), "rm" (frame->arg_1), "rm" (frame->arg_2), "rm" (frame->arg_3), "rm" (frame->arg_4), "rm" (frame->arg_5), "rm" (frame->arg_6) 
-    : "rax", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "memory");
-  return (int)result;
-} 
 
 void put_bytes_in_arr(char* arr, unsigned long long val) {
   for(int i = 0; i < 8;i++) {
@@ -50,35 +19,25 @@ void put_bytes_in_arr(char* arr, unsigned long long val) {
   }
 }
 
-int tyche_domain_attestation(usize nonce, hello_world_t* ans, int mode) {
-  vmcall_frame_t frame = {
-    .vmcall = TYCHE_ENCLAVE_ATTESTATION,
-    .arg_1 = nonce,
-    .arg_2 = mode,
-  };
-  if (tyche_call_enclave(&frame) != SUCCESS) {
-    goto failure;
-  }
+void tyche_call_wrapper(usize nonce, hello_world_t* ans, int mode) {
+  unsigned long long vals[6];
+  tyche_domain_attestation(nonce, vals, mode);
   if(mode == CALC_REPORT) {
-    put_bytes_in_arr(ans->pub_key, frame.value_1);
-    put_bytes_in_arr(ans->pub_key + 8, frame.value_2);
-    put_bytes_in_arr(ans->pub_key + 16, frame.value_3);
-    put_bytes_in_arr(ans->pub_key + 24, frame.value_4);
-    put_bytes_in_arr(ans->signed_enclave_data, frame.value_5);
-    put_bytes_in_arr(ans->signed_enclave_data + 8, frame.value_6);
+    put_bytes_in_arr(ans->pub_key, vals[0]);
+    put_bytes_in_arr(ans->pub_key + 8, vals[1]);
+    put_bytes_in_arr(ans->pub_key + 16, vals[2]);
+    put_bytes_in_arr(ans->pub_key + 24, vals[3]);
+    put_bytes_in_arr(ans->signed_enclave_data, vals[4]);
+    put_bytes_in_arr(ans->signed_enclave_data + 8, vals[5]);
   }
   else if(mode == READ_REPORT) {
-    put_bytes_in_arr(ans->signed_enclave_data + 16, frame.value_1);
-    put_bytes_in_arr(ans->signed_enclave_data + 24, frame.value_2);
-    put_bytes_in_arr(ans->signed_enclave_data + 32, frame.value_3);
-    put_bytes_in_arr(ans->signed_enclave_data + 40, frame.value_4);
-    put_bytes_in_arr(ans->signed_enclave_data + 48, frame.value_5);
-    put_bytes_in_arr(ans->signed_enclave_data + 56, frame.value_6);
+    put_bytes_in_arr(ans->signed_enclave_data + 16, vals[0]);
+    put_bytes_in_arr(ans->signed_enclave_data + 24, vals[1]);
+    put_bytes_in_arr(ans->signed_enclave_data + 32, vals[2]);
+    put_bytes_in_arr(ans->signed_enclave_data + 40, vals[3]);
+    put_bytes_in_arr(ans->signed_enclave_data + 48, vals[4]);
+    put_bytes_in_arr(ans->signed_enclave_data + 56, vals[5]);
   }
-  
-  return SUCCESS;
-failure:
-  return FAILURE;
 }
 
 void my_memcpy(void* dest, void* src, int size)
@@ -109,8 +68,8 @@ void hello_world(frame_t* frame)
   gate_call(frame);
 
   nonce_t nonce = msg->nonce;
-  tyche_domain_attestation(nonce, msg, CALC_REPORT);
-  tyche_domain_attestation(nonce, msg, READ_REPORT);
+  tyche_call_wrapper(nonce, msg, CALC_REPORT);
+  tyche_call_wrapper(nonce, msg, READ_REPORT);
   print_message((void*)message3, 20);
 }
 
