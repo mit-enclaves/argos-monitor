803743ccc5678b492ae28b4f076893ff87e3cd25
papa <pavle.jane@gmail.com>
2023-08-16 13:33:12 +0200

    Porting even more functions into bricks - trusted entry - trusted main is just a wrapper for bricks trusted main now, cannot call bricks trusted main directly from asm?

--- C/libraries/tyche-trusted-runtime/include/bricks.h
+++ C/libraries/tyche-trusted-runtime/include/bricks.h
@@ -3,9 +3,10 @@
 
 #include "sdk_tyche_rt.h"
 
-extern int bricks_gate_call(frame_t* frame);
+// extern int bricks_gate_call(frame_t* frame);
+extern int bricks_gate_call();
 extern int bricks_function(int a, int b);
-extern bricks_asm_call_gate(capa_index_t* capa, void** args);
 extern void* bricks_get_default_shared_buffer();
-
+extern void bricks_trusted_entry(frame_t* frame);
+extern void bricks_trusted_main(capa_index_t ret_handle, void* args);
 #endif
