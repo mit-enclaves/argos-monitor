e23174874bacd9f116b53b517b88a75476823b5f
papa <pavle.jane@gmail.com>
2023-08-17 16:57:00 +0200

    Starting to implement few system calls - will be tested through ring 0 - print - attest enclave - edge call - go to untrusted part User mode testing ?

--- crates/bricks/src/syscall_handlers.rs
+++ crates/bricks/src/syscall_handlers.rs
@@ -1,5 +1,6 @@
 use core::arch::asm;
 
+use crate::bricks_const::{FAILURE, SUCCESS};
 use crate::syscalls;
 
 #[no_mangle]
@@ -18,16 +19,38 @@ pub extern "C" fn bricks_syscall_handler() {
         asm!("mov {:}, rsi", out(reg) rsi);
         asm!("mov {:}, rdx", out(reg) rdx);
     }
-
+    let mut result: u32 = FAILURE;
     match rax {
         syscalls::ATTEST_ENCLAVE => {
+            result = bricks_attest_enclave_handler();
+        }
+        syscalls::PRINT => {
             // TODO implement it
         }
-        syscalls::LINUX_MALLOC => {
+        syscalls::EDGE_CALL => {
+            result = bricks_edge_call_handler();
+        }
+        syscalls::LINUX_MMAP => {
             // TODO implement it
         }
         _ => {
             // TODO implement it
         }
     }
+    // TODO return result to user code
+}
+
+// TODO
+pub fn bricks_attest_enclave_handler() -> u32 {
+    SUCCESS
+}
+
+use crate::gate_calls::bricks_gate_call;
+pub fn bricks_edge_call_handler() -> u32 {
+    bricks_gate_call()
+}
+
+// TODO
+pub fn bricks_print_handler() -> u32 {
+    SUCCESS
 }
