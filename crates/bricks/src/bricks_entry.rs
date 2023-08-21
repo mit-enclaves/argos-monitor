5e11a0cce06637e9f02c2c5745f9656ad9e33c9a
papa <pavle.jane@gmail.com>
2023-08-21 17:29:40 +0200

    Code cleanup

--- crates/bricks/src/bricks_entry.rs
+++ crates/bricks/src/bricks_entry.rs
@@ -1,31 +1,15 @@
-use core::arch::asm;
-
-use lazy_static::lazy_static;
-
 use crate::gate_calls::{current_frame, BricksFrame};
 use crate::gdt::bricks_init_gdt;
 use crate::idt::bricks_init_idt;
 use crate::syscall_handlers::{bricks_save_syscalls, bricks_syscalls_init};
-
-extern "C" {
-    fn setup_interrupts_syscalls();
-    fn syscall_init();
-}
 
 pub fn bricks_interrupt_setup() {
     bricks_init_gdt();
     bricks_init_idt();
 }
 
-pub fn c_interrupt_setup() {
-    unsafe {
-        setup_interrupts_syscalls();
-    }
-}
-
 pub fn interrupt_setup() {
     bricks_interrupt_setup();
-    // c_interrupt_setup();
 }
 
 pub fn syscall_setup() {
