d6309086a098b064f6258e3d3eca48b07163bae9
papa <pavle.jane@gmail.com>
2023-08-21 16:08:08 +0200

    - Adding calls to write and read bytes to shared buffer from enclave - writing some util functions - code refactoring Next step: fixing syscall return and memory management

--- C/libraries/tyche-trusted-runtime/runtime/lib.c
+++ C/libraries/tyche-trusted-runtime/runtime/lib.c
@@ -11,33 +11,3 @@ void trusted_main(capa_index_t ret_handle, void *args)
   bricks_trusted_main(ret_handle, args);
 }
 
-// ———————————————————————————— Functions ported to Bricks ———————————————————————————— //
-
-// void old_trusted_main(capa_index_t ret_handle, void* args) {
-  // frame_t frame = {ret_handle, args}; 
- 
-  // Call the domain's main.
-  // trusted_entry(&frame); 
-  // bricks_trusted_entry(&frame);
-
-  // Done executing the domain, return.
-  // bricks_gate_call();
-  // gate_call(&frame);
-  // Should never return, if we do, an exit call happens.
-// }
-
-// extern int asm_call_gate(capa_index_t* capa, void** args);
-// int gate_call(frame_t* frame)
-// {
-//   usize result = FAILURE;
-//   result = asm_call_gate(&(frame->ret_handle), &(frame->args));
-//   return result;
-// }
-
-/// This is introduced by tychools.
-// char *shared_buffer = (char*) 0x300000; 
-// void* get_default_shared_buffer()
-// {
-//   return (void*) (shared_buffer);
-// }
-
