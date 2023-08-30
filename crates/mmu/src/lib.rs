0e3aa516ac417dd97b15aa0c3303884dda3a0e9f
Neelu Kalani <neelukalani7@gmail.com>
2023-08-30 14:34:33 +0200

    mmu crate - added feature riscv_enabled

--- crates/mmu/src/lib.rs
+++ crates/mmu/src/lib.rs
@@ -3,16 +3,14 @@
 
 pub mod frame_allocator;
 pub mod ptmapper;
+pub mod ioptmapper;
 pub mod walker;
 
 pub use frame_allocator::{FrameAllocator, RangeAllocator};
+pub use ioptmapper::{IoPtFlag, IoPtMapper};
 pub use ptmapper::{PtFlag, PtMapper};
 
 // ————————————————————————————————— x86_64 ————————————————————————————————— //
-#[cfg(target_arch = "x86_64")]
-pub mod ioptmapper;
-#[cfg(target_arch = "x86_64")]
-pub use ioptmapper::{IoPtFlag, IoPtMapper};
 #[cfg(target_arch = "x86_64")]
 pub mod eptmapper;
 #[cfg(target_arch = "x86_64")]
