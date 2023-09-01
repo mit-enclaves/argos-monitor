2363747000f9114b33748ac3fa7ad7d9ab123fb8
papa <pavle.jane@gmail.com>
2023-09-01 13:00:29 +0200

    Pushing just to save small modifications, have to check something on interrupts

--- crates/bricks/src/x86_64/gdt.rs
+++ crates/bricks/src/x86_64/gdt.rs
@@ -11,6 +11,7 @@ pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
+        // tss.privilege_stack_table
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
