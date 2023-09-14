8d67781924d182f40d6b01d77695ac484d3fc8e3
Adrien Ghosn <ghosn.adrien@gmail.com>
2023-09-14 12:51:26 +0100

    Added filtering on cpuid for tpause
    Apparently now I run into pkru issues.
    I will create another commit that extracts cpuid and puts it in
    a separate function will all the necessary filtering.

--- monitor/tyche/src/x86_64/guest.rs
+++ monitor/tyche/src/x86_64/guest.rs
@@ -3,10 +3,11 @@
 use core::arch::asm;
 
 use capa_engine::{Bitmaps, Domain, Handle, LocalCapa, NextCapaToken};
-use vmx::bitmaps::exit_qualification;
+use vmx::bitmaps::{exit_qualification, SecondaryControls};
 use vmx::errors::Trapnr;
 use vmx::{
     ActiveVmcs, ControlRegister, InterruptionType, Register, VmExitInterrupt, VmxExitReason, Vmxon,
+    CPUID_ECX_X64_WAITPGK,
 };
 
 use super::{cpuid, monitor};
@@ -383,7 +384,7 @@ fn handle_exit(
             let input_ecx = vs.vcpu.get(Register::Rcx);
             let eax: u64;
             let ebx: u64;
-            let ecx: u64;
+            let mut ecx: u64;
             let edx: u64;
 
             unsafe {
@@ -400,6 +401,21 @@ fn handle_exit(
                     out("rdx") edx,
                     out("rsi") ebx
                 )
+            }
+            // Some filtering according to supported native features and virtualization ones.
+            // For example, 13th Gen Intel(R) Core(TM) i5-1345U support tpause and invpcid natively
+            // but does not allow the secondary control bit 26 to allow tpause in guest.
+            // TODO: put that in a separate function.
+            if (input_eax == 0x7 && input_ecx == 0x0)
+                && (vmx::secondary_controls_capabilities()
+                    .expect("failed to get secondary capabilities")
+                    .bits()
+                    & SecondaryControls::ENABLE_USER_WAIT_PAUSE.bits()
+                    == 0)
+                && (ecx & (CPUID_ECX_X64_WAITPGK as u64) != 0)
+            {
+                // clear the cpuid tpause bit.
+                ecx = ecx ^ !(CPUID_ECX_X64_WAITPGK as u64);
             }
 
             vs.vcpu.set(Register::Rax, eax);
