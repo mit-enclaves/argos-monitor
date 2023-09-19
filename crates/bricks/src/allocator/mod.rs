c5bd02bd7e82dc48c9b6f701ec4272cc75d8d17d
papa <pavle.jane@gmail.com>
2023-09-19 17:08:03 +0200

    Fixing PR comments
    - Removing bricks allocator
    - Allocators return Result<>
    - unsafe util functions
    - TODO: unsafe blocks - write assumptions for safety mostly

--- crates/bricks/src/allocator/mod.rs
+++ crates/bricks/src/allocator/mod.rs
@@ -1,13 +1,9 @@
-use self::bricks_allocator::BricksAllocator;
 use self::user_allocator::UserAllocator;
 use crate::arch::VirtualAddr;
 
-pub mod bricks_allocator;
 pub mod page_allocator;
 pub mod user_allocator;
 pub mod utils;
-
-static mut BRICKS_ALLOCATOR: BricksAllocator = BricksAllocator::new();
 
 static mut USER_ALLOCATOR: UserAllocator = UserAllocator::new();
 
@@ -15,14 +11,6 @@ pub fn alloc_user(num_bytes: u64) -> u64 {
     unsafe { USER_ALLOCATOR.malloc(num_bytes).as_u64() }
 }
 
-pub fn alloc_bricks(num_bytes: u64) -> (bool, VirtualAddr) {
-    unsafe { BRICKS_ALLOCATOR.kmalloc(num_bytes) }
-}
-
 pub fn free_user(addr: VirtualAddr) -> u64 {
     unsafe { USER_ALLOCATOR.free(addr).as_u64() }
-}
-
-pub fn free_bricks(addr: VirtualAddr) -> bool {
-    unsafe { BRICKS_ALLOCATOR.kfree(addr) }
 }
