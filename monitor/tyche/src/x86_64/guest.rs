8b7a083444771732b31f51e89f3790cc7bca9bc7
YuChen Qian <yuchenqian96@gmail.com>
2023-11-03 10:56:20 +0100

    I/O: create I/O domain from hypercall
    This is a shortcut to create an I/O domain from the dom0 guest through a
    hypercall. It would probably be much better to have a global I/O domain
    right inside tyche, but I have a bit trouble getting the domain handler
    on the guest kernel side. As a result, I opt to add this commit for now
    (and probably revert this one later).
    
    Signed-off-by: YuChen Qian <yuchenqian96@gmail.com>

--- monitor/tyche/src/x86_64/guest.rs
+++ monitor/tyche/src/x86_64/guest.rs
@@ -95,7 +95,8 @@ fn handle_exit(
             match vmcall {
                 calls::CREATE_DOMAIN => {
                     log::trace!("Create Domain");
-                    let capa = monitor::do_create_domain(*domain).expect("TODO");
+                    let io = arg_1 != 0;
+                    let capa = monitor::do_create_domain(*domain, io).expect("TODO");
                     vs.vcpu.set(Register::Rdi, capa.as_u64());
                     vs.vcpu.set(Register::Rax, 0);
                     vs.vcpu.next_instruction()?;
