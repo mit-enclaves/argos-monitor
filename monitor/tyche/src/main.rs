f8422fb7c586595fe45f60172305b889fa545c33
Neelu Kalani <neelukalani7@gmail.com>
2023-11-10 15:38:45 +0100

    Running Tyche on the VF2 Board - initial commit

--- monitor/tyche/src/main.rs
+++ monitor/tyche/src/main.rs
@@ -11,6 +11,8 @@ use tyche;
 use tyche::debug::qemu;
 use tyche::{arch, println};
 
+use core::arch::asm; 
+
 entry_point!(tyche_entry_point);
 
 const LOG_LEVEL: LevelFilter = LevelFilter::Info;
@@ -20,9 +22,22 @@ fn tyche_entry_point() -> ! {
     arch::arch_entry_point(LOG_LEVEL);
 }
 
-#[cfg(target_arch = "riscv64")]
+#[cfg(all(target_arch = "riscv64"), not(feature = "visionfive2"))]
 fn tyche_entry_point(hartid: usize, manifest: RVManifest) -> ! {
     arch::arch_entry_point(hartid, manifest, LOG_LEVEL);
+}
+
+#[cfg(all(target_arch = "riscv64"), feature = "visionfive2")]
+fn tyche_entry_point(hartid: u64, arg1: u64, next_addr: u64, next_mode: u64) -> ! {
+    use tyche::riscv::launch_guest;
+
+    unsafe { 
+        asm!("ecall");
+    }
+    //logger::init(LOG_LEVEL);
+    //arch::arch_entry_point(hartid, arg1, next_addr, next_mode, LOG_LEVEL);
+    launch_guest(hartid, arg1, next_addr, next_mode);
+    qemu::exit(qemu::ExitCode::Success); 
 }
 
 #[panic_handler]
