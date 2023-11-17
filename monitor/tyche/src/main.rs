7fb481595793a3b177a129632bc9cb399358d5f4
Neelu Kalani <neelukalani7@gmail.com>
2023-11-17 16:21:53 +0100

    Conditional ecall on hartid check

--- monitor/tyche/src/main.rs
+++ monitor/tyche/src/main.rs
@@ -31,11 +31,13 @@ fn tyche_entry_point(hartid: usize, manifest: RVManifest) -> ! {
 fn tyche_entry_point(hartid: u64, arg1: u64, next_addr: u64, next_mode: u64) -> ! {
     use tyche::riscv::launch_guest;
 
-    unsafe { 
-        asm!("ecall");
+    if hartid != 1 {
+        unsafe { 
+            asm!("ecall");
+        }
     }
 
-    println!("============== HELLO FROM TYCHE ===============");
+    //println!("============== HELLO FROM TYCHE ===============");
     //logger::init(LOG_LEVEL);
     //arch::arch_entry_point(hartid, arg1, next_addr, next_mode, LOG_LEVEL);
     launch_guest(hartid, arg1, next_addr, next_mode);
