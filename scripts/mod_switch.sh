f8a3680890e70d2070a76a4245e3a83f5e9d9846
Adrien Ghosn <ghosn.adrien@gmail.com>
2024-03-06 15:01:52 +0000

    Justifle rule to easily setup lab environment on x86
    The new setup_lab_x86 justfile should generate a nice work environment
    in the TDO linux disk rooted at /tyche.
    It also copies a README that explains how to run benchmarks.

--- scripts/mod_switch.sh
+++ scripts/mod_switch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo modprobe -r kvm_intel
-sudo modprobe kvm_themis
+if [[ $(lsmod | grep kvm_intel) ]]; then
+  sudo modprobe -r kvm_intel
+fi
+if [[ $(lsmod | grep kvm_themis) ]]; then
+  echo "kvm_themis already loaded."
+else
+  sudo modprobe kvm_themis
+fi
