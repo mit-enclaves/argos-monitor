dd21be1df310da980fc4053ba4ca10708b7a50d8
Neelu Kalani <neelukalani7@gmail.com>
2024-03-31 13:12:38 +0200

    Emulated misaligned stores

--- crates/riscv_csrs/src/lib.rs
+++ crates/riscv_csrs/src/lib.rs
@@ -16,6 +16,7 @@ pub mod mcause {
     pub const ILLEGAL_INSTRUCTION: usize = 2;
     pub const LOAD_ADDRESS_MISALIGNED: usize = 4;
     pub const LOAD_ACCESS_FAULT: usize = 5;
+    pub const STORE_ADDRESS_MISALIGNED: usize = 6;
     pub const ECALL_FROM_UMODE: usize = 8;
     pub const STORE_ACCESS_FAULT: usize = 7;
     pub const ECALL_FROM_SMODE: usize = 9;
