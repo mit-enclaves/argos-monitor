890f2868bee22c949d746bb0f1800fbf9a0941a7
Adrien Ghosn <ghosn.adrien@gmail.com>
2024-04-08 16:32:35 +0100

    Simplifying benchmarks
    Simplifying the benchmarks with tcp.
    We accept only one client, removed the prints, and added non-blocking
    sockets.

--- C/libraries/sdktyche/example/redis/untrusted/tcp_server.c
+++ C/libraries/sdktyche/example/redis/untrusted/tcp_server.c
@@ -20,8 +20,6 @@
 typedef struct thread_arg_t {
   // Client socket.
   int socket;
-  // Pointer to the mutex for the channel.
-  pthread_mutex_t *mutex;
   // the redis_app info
   redis_app_t* app;
 } thread_arg_t;
@@ -31,24 +29,23 @@ void *tcp_connection_handler(void *arg) {
   thread_arg_t *arguments = (thread_arg_t*) arg;
   int new_socket = arguments->socket;
   char buffer[NET_BUFFER_SIZE] = {0};
-  ssize_t valread;
-  pthread_t reader;
 
   LOG("Started a TCP connexion handler for a client");
   while(1) {
     int written = 0;
     int read = recv(new_socket, buffer, NET_BUFFER_SIZE, MSG_DONTWAIT);
-    if (read == -1) {
+    if (read < 0) {
       if (errno != EAGAIN && errno != EWOULDBLOCK) {
         ERROR("Something went wrong with the socket");
-        return NULL;
+        exit(-1);
       }
+      read = 0;
       goto try_read;
     }
     if (read == 0) {
       goto try_read;
     }
-    LOG("Received %d bytes", read);
+
     // We have some things to transmit to ssl.
     written = rb_char_write_n(&(arguments->app->to_redis), read, buffer);
     if (written == FAILURE || written != read) {
@@ -58,19 +55,14 @@ void *tcp_connection_handler(void *arg) {
       }
       ERROR("failed to write everything to ssl %d, expected %d | %d is full",
           written, read, rb_char_is_full(&(arguments->app->to_redis)));
-      return NULL;
+      exit(-1);
     }
-    LOG("Wrote %d bytes on the ssl request", written);
-
 try_read:
     written = 0;
     read = rb_char_read_n(&(arguments->app->from_redis), MSG_BUFFER_SIZE, buffer);
     if (read == FAILURE) {
       ERROR("Error reading from ssl");
       exit(-1);
-    }
-    if (read > 0) {
-      LOG("Read %d bytes from ssl response", read);
     }
     while (written < read) {
       int res = write(new_socket, buffer, read - written);
@@ -80,57 +72,9 @@ try_read:
       }
       written += res;
     }
-    if (read > 0) 
-      LOG("Wrote %d to tcp", written);
   }
-
-  // Previous implementation that was blocking.
-  /*
-  while ((valread = read(new_socket, buffer, NET_BUFFER_SIZE)) > 0) {
-    // Lock the channels.
-    pthread_mutex_lock(arguments->mutex);
-    //TODO: let's figure things out to have the size.
-    int res = rb_char_write_n(&(arguments->app->to_redis), valread, buffer);
-    if (res == FAILURE || res != valread) {
-        ERROR("Could not write everything to redis");
-        pthread_mutex_unlock(arguments->mutex);
-        goto finish;
-    }
-
-    // Now read the reply.
-    do {
-      res = rb_char_read_n(&(arguments->app->from_redis), MSG_BUFFER_SIZE, buffer); 
-      if (res == FAILURE) {
-        ERROR("Error reading from redis");
-        pthread_mutex_unlock(arguments->mutex);
-        goto finish;
-      }
-    } while(res == 0);
-
-    // Unlock.
-    pthread_mutex_unlock(arguments->mutex);
-    int written = 0;
-    int to_write = res;
-    while (written < to_write) {
-      int res = write(new_socket, buffer, to_write - written);
-      if (res < 0) {
-        ERROR("Failed to write back the response");
-        goto finish;
-      }
-      written += res;
-    }
-  }
-
-  if (valread == 0) {
-    printf("Client disconnected\n");
-  } else {
-    perror("read");
-  }*/
-
-finish:
-  close(new_socket);
-  free(arguments);
-  pthread_exit(NULL);
+  // Should never reach but...
+  return NULL;
 }
 
 int tcp_start_server(usize core, redis_app_t* comm) {
@@ -138,8 +82,6 @@ int tcp_start_server(usize core, redis_app_t* comm) {
   struct sockaddr_in address;
   int opt = 1;
   int addrlen = sizeof(address);
-  pthread_mutex_t mutex;
-  pthread_mutex_init(&mutex, NULL);
 
   // Creating socket file descriptor
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -169,40 +111,24 @@ int tcp_start_server(usize core, redis_app_t* comm) {
 
   // Accept connections and handle each in a new thread
   LOG("Started TCP server on %d", NET_PORT);
-  while (1) {
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0) {
-      perror("accept");
-      continue;
-    }
-
-    pthread_t thread;
-    thread_arg_t* args = malloc(sizeof(thread_arg_t));
-    if (args == NULL) {
-      ERROR("Unable to allocate thread args.");
-      close(new_socket);
-      exit(-1);
-    }
-    args->socket = new_socket;
-    args->mutex = &mutex;
-    args->app = comm;
-
-
-    // Allow only a single client.
-    tcp_connection_handler((void*) args);
-    LOG("Client disconnected");
-    break;
-    /*if (pthread_create(&thread, NULL, tcp_connection_handler, (void *)args) <
-        0) {
-      perror("pthread_create");
-      free(args);
-      close(new_socket);
-      continue;
-    }
-
-    // Detach the thread, so its resources are automatically released when it
-    // finishes
-    pthread_detach(thread);*/
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t *)&addrlen)) < 0) {
+    return FAILURE;
   }
+  thread_arg_t* args = malloc(sizeof(thread_arg_t));
+  if (args == NULL) {
+    ERROR("Unable to allocate thread args.");
+    close(new_socket);
+    exit(-1);
+  }
+  args->socket = new_socket;
+  args->app = comm;
+
+
+  // Allow only a single client.
+  tcp_connection_handler((void*) args);
+  LOG("Client disconnected");
+  free(args);
   close(server_fd);
+  return SUCCESS;
 }
--- C/libraries/sdktyche/example/ssl-redis/untrusted/tcp_server.c
+++ C/libraries/sdktyche/example/ssl-redis/untrusted/tcp_server.c
@@ -24,8 +24,6 @@
 typedef struct thread_arg_t {
   // Client socket.
   int socket;
-  // Pointer to the mutex for the channel.
-  pthread_mutex_t *mutex;
   // the redis_app info
   ssl_channel_t* app;
 } thread_arg_t;
@@ -44,17 +42,16 @@ void *tcp_connection_handler(void *arg) {
   while(1) {
     int written = 0;
     int read = recv(new_socket, buffer, NET_BUFFER_SIZE, MSG_DONTWAIT);
-    if (read == -1) {
+    if (read < 0) {
       if (errno != EAGAIN && errno != EWOULDBLOCK) {
         ERROR("Something went wrong with the socket");
-        return NULL;
+        exit(-1);
       }
       goto try_read;
     }
     if (read == 0) {
       goto try_read;
     }
-    LOG("Received %d bytes", read);
     // We have some things to transmit to ssl.
     written = rb_char_write_n(&(arguments->app->request), read, buffer);
     if (written == FAILURE || written != read) {
@@ -64,9 +61,8 @@ void *tcp_connection_handler(void *arg) {
       }
       ERROR("failed to write everything to ssl %d, expected %d | %d is full",
           written, read, rb_char_is_full(&(arguments->app->request)));
-      return NULL;
+      exit(-1);
     }
-    LOG("Wrote %d bytes on the ssl request", written);
 
 try_read:
     written = 0;
@@ -74,9 +70,6 @@ try_read:
     if (read == FAILURE) {
       ERROR("Error reading from ssl");
       exit(-1);
-    }
-    if (read > 0) {
-      LOG("Read %d bytes from ssl response", read);
     }
     while (written < read) {
       int res = write(new_socket, buffer, read - written);
@@ -86,59 +79,8 @@ try_read:
       }
       written += res;
     }
-    if (read > 0) 
-      LOG("Wrote %d to tcp", written);
   }
-
-  // Previous implementation that was 1 thread per client.
-  // It is blocking however and needs to be fixed.
-  // For now run one client to completion.
-  /*
-  while ((valread = read(new_socket, buffer, NET_BUFFER_SIZE)) > 0) {
-    // Lock the channels.
-    pthread_mutex_lock(arguments->mutex);
-    //TODO: let's figure things out to have the size.
-    LOG("Writting %ld bytes into the ssl request channel.", valread);
-    int res = rb_char_write_n(&(arguments->app->request), valread, buffer);
-    if (res == FAILURE || res != valread) {
-        ERROR("Could not write everything to redis");
-        pthread_mutex_unlock(arguments->mutex);
-        goto finish;
-    }
-
-    // Now read the reply.
-    res = rb_char_read_n(&(arguments->app->response), MSG_BUFFER_SIZE, buffer);
-    LOG("Read %d bytes", res);
-    if (res == FAILURE) {
-      ERROR("Error reading from ssl");
-      pthread_mutex_unlock(arguments->mutex);
-      goto finish;
-    }
-
-    // Unlock.
-    pthread_mutex_unlock(arguments->mutex);
-    int written = 0;
-    int to_write = res;
-    while (written < to_write) {
-      int res = write(new_socket, buffer, to_write - written);
-      if (res < 0) {
-        ERROR("Failed to write back the response");
-        goto finish;
-      }
-      written += res;
-    }
-  }
-
-  if (valread == 0) {
-    printf("Client disconnected\n");
-  } else {
-    perror("read");
-  }
-
-finish:
-  close(new_socket);
-  free(arguments);
-  pthread_exit(NULL);*/
+  return NULL;
 }
 
 int tcp_start_server(usize core, ssl_channel_t* comm) {
@@ -146,8 +88,6 @@ int tcp_start_server(usize core, ssl_channel_t* comm) {
   struct sockaddr_in address;
   int opt = 1;
   int addrlen = sizeof(address);
-  pthread_mutex_t mutex;
-  pthread_mutex_init(&mutex, NULL);
 
   // Creating socket file descriptor
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -177,39 +117,24 @@ int tcp_start_server(usize core, ssl_channel_t* comm) {
 
   // Accept connections and handle each in a new thread
   LOG("Started TCP server on %d", NET_PORT);
-  while (1) {
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0) {
-      perror("accept");
-      continue;
-    }
-
-    pthread_t thread;
-    thread_arg_t* args = malloc(sizeof(thread_arg_t*));
-    if (args == NULL) {
-      ERROR("Unable to allocate arguments for the connection handler threads.");
-      exit(-1);
-    }
-
-    // Setup the arguments for tcp.
-    args->socket = new_socket;
-    args->mutex = &mutex;
-    args->app = comm;
-
-    // Previous implementation with threads.
-    // For now run the handler directly.
-    /*if (pthread_create(&thread, NULL, tcp_connection_handler, (void *)args) <
-        0) {
-      perror("pthread_create");
-      free(args);
-      close(new_socket);
-      continue;
-    }*/
-    tcp_connection_handler((void*) args);
-    LOG("Returned from handling client");
-    exit(-1);
-    // Detach the thread, so its resources are automatically released when it
-    // finishes
-    //pthread_detach(thread);
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t *)&addrlen)) < 0) {
+    ERROR("Failed accept");
+    return FAILURE;
   }
+  pthread_t thread;
+  thread_arg_t* args = malloc(sizeof(thread_arg_t*));
+  if (args == NULL) {
+    ERROR("Unable to allocate arguments for the connection handler threads.");
+    exit(-1);
+  }
+  // Setup the arguments for tcp.
+  args->socket = new_socket;
+  args->app = comm;
+
+  tcp_connection_handler((void*) args);
+  LOG("Returned from handling client");
+  free(args);
+  close(server_fd);
+  return SUCCESS;
 }
