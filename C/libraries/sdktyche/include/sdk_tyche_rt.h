c48148cfca8e4571057fccf42216335a2763c22a
Adrien Ghosn <ghosn.adrien@gmail.com>
2024-04-09 23:56:24 +0100

    Fixing transition benchmark
    I found a way to have the right handle.
    I enumerate on the first call (which is a warmup call).
    This should be working now.
    
    TODO: We can use the same trick within the benchmark to remove the syscall
    overhead.

--- C/libraries/sdktyche/include/sdk_tyche_rt.h
+++ C/libraries/sdktyche/include/sdk_tyche_rt.h
@@ -8,5 +8,6 @@
 
 // —————————————————————————————————— API ——————————————————————————————————— //
 int gate_call(void);
+capa_index_t find_switch(void);
 void trusted_main(void);
 void* get_default_shared_buffer();
--- C/microbenchmarks/transition/main.c
+++ C/microbenchmarks/transition/main.c
@@ -1,8 +1,12 @@
+#include "sdk_tyche_rt.h"
+
 extern void fast_call_gate(unsigned long long t);
 
 void trusted_entry(void)
 {
+	// Hopefully the capa index does not change after the first run.
+  unsigned long long capa = find_switch();
 	while(1) {
-		fast_call_gate(0xdeadbeef);
+		fast_call_gate(capa);
 	} 
 }
