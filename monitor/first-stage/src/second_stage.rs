379c4f562f8561ac1f46434f3b9b23321edd6318
Jules Drean <drean@mit.edu>
2024-10-28 16:48:22 +0000

    Changes hardcoded constants to run SealPIR

--- monitor/first-stage/src/second_stage.rs
+++ monitor/first-stage/src/second_stage.rs
@@ -27,7 +27,7 @@ const SECOND_STAGE_SIZE: usize = 0x1000 * 2048;
 const LOAD_VIRT_ADDR: HostVirtAddr = HostVirtAddr::new(0x80000000000);
 //  Stack definitions
 const STACK_VIRT_ADDR: HostVirtAddr = HostVirtAddr::new(0x90000000000);
-const STACK_SIZE: usize = 0x1000 * 16;
+const STACK_SIZE: usize = 0x1000 * 5;
 
 /// Second stage jump structures
 static mut SECOND_STAGE_ENTRIES: [Option<Stage2>; MAX_CPU_NUM] = [None; MAX_CPU_NUM];
