d836b7be622bfb7d3f3266183507d0c3d361913b
Fisher Jepsen <efjepsen@mit.edu>
2024-11-01 15:17:15 +0000

    [hwfix] add tyche reserved memory bootargs to bare_metal builds

--- monitor/first-stage/src/guests/linux.rs
+++ monitor/first-stage/src/guests/linux.rs
@@ -33,7 +33,7 @@ static COMMAND_LINE: &'static [u8] =
     b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0 console=ttyS0 iommu=on iommu.passthrough=1 intremap=nopost iommu.strict=1 tyche.use_reserved_memory=1 tyche.reserved_base=0x200000000 tyche.reserved_size=8 memmap=8G$0x200000000\0";
 #[cfg(feature = "bare_metal")]
 static COMMAND_LINE: &'static [u8] =
-    b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0,115200 console=ttyS0,115200\0";
+    b"root=/dev/sdb2 apic=debug earlyprintk=serial,ttyS0,115200 console=ttyS0,115200 tyche.use_reserved_memory=1 tyche.reserved_base=0x200000000 tyche.reserved_size=8 memmap=8G$0x200000000\0\0";
 
 pub struct Linux {}
 
